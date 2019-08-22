944a85c180e5a39435d762176c3c9794b1e9a2bc
Yuxin Gao <Castlebin@gmail.com>
2019-08-22 16:45:46 +0800

    Record-Replay-Verification

--- src/test/java/com/heller/jmockit/ProgramConstructureTest.java
+++ src/test/java/com/heller/jmockit/ProgramConstructureTest.java
@@ -11,7 +11,16 @@ import org.junit.Test;
 /**
  * 通过下面例子可以看出，
  * JMockit的程序结构包含了测试属性或测试参数，测试方法，
- * 测试方法体中又包含 录制代码块，重放测试逻辑，验证代码块  三大块。
+ * 测试方法体中又包含 录制代码块，重放测试逻辑，验证代码块  三大块，即所谓的（Record-Replay-Verification）。
+ *
+ * Record-Replay-Verification 是JMockit测试程序的主要结构。
+ * Record: 即先录制某类/对象的某个方法调用，在当输入什么时，返回什么。
+ * Replay: 即重放测试逻辑。
+ * Verification: 重放后的验证。比如验证某个方法有没有被调用，调用多少次。
+ *
+ * 其实，Record-Replay-Verification与JUnit程序的AAA(Arrange-Action-Assert)结构是一样的。
+ * Record对应Arrange，先准备一些测试数据，测试依赖。Replay对应Action，即执行测试逻辑。Verification对应Assert，即做测试验证。
+ *
  *
  * @Mocked
  * 该注解修饰一个测试属性或者参数，表示该对象的的实例化，属性赋值，方法调用的返回值全部由JMockit来接管，
